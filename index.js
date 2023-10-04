79fc3c58a94d2283d5893d80c8b340b3ead9b6dd
TamaDitya <cikoadityapratama1@gmail.com>
2023-10-04 15:23:55 +0800

    second commit : delete the body parser import line code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
